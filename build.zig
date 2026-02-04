4ce2a6a95d4410573dcc1c159085f554465aea04
Khitiara <robotbrainify@gmail.com>
2026-02-03 22:32:42 -0500

    rebase and fixup

--- build.zig
+++ build.zig
@@ -20,37 +20,37 @@ pub fn build(b: *std.Build) void {
     const config = blk: {
         var config = Config{};
 
-        add_config_option(b, &config, .read_only, "If set, the library will only be able to read filesystems.");
-        add_config_option(b, &config, .minimize, "Set this to different values to reduce API surface");
-        add_config_option(b, &config, .find, "Enable find support");
-        add_config_option(b, &config, .mkfs, "Enable mkfs support");
-        add_config_option(b, &config, .fastseek, "Use fast seek option");
-        add_config_option(b, &config, .expand, "Enables expandfs support");
-        add_config_option(b, &config, .chmod, "Enables chmod support");
-        add_config_option(b, &config, .label, "Enables support for handling of filesystem labels");
-        add_config_option(b, &config, .forward, "Enables forward support");
-        add_config_option(b, &config, .strfuncs, "Enables file string functions");
-        add_config_option(b, &config, .printf_lli, "Enables printf_lli support");
-        add_config_option(b, &config, .printf_float, "Enables printf_float support");
-        add_config_option(b, &config, .strf_encoding, "Sets the format string encoding");
-        add_config_option(b, &config, .max_long_name_len, "Sets the maximum name for long file names");
-        add_config_option(b, &config, .code_page, "Defines the OEM code page");
-        add_config_option(b, &config, .long_file_name, "Enables long file name support");
-        add_config_option(b, &config, .long_file_name_encoding, "Sets the encoding for long file names");
-        add_config_option(b, &config, .long_file_name_buffer_size, "Sets the buffer size for long file names");
-        add_config_option(b, &config, .short_file_name_buffer_size, "Sets the buffer size for short file names");
-        add_config_option(b, &config, .relative_path_api, "Enables the relative path API");
-        add_config_option(b, &config, .multi_partition, "Enables support for several partitions on the same drive");
-        add_config_option(b, &config, .lba64, "Enables support for 64 bit linear block addresses.");
-        add_config_option(b, &config, .min_gpt_sectors, "Sector count threshold for switching to GPT partition tables");
-        add_config_option(b, &config, .use_trim, "Enables support for ATA TRIM command");
-        add_config_option(b, &config, .tiny, "Enable tiny buffer configuration");
-        add_config_option(b, &config, .exfat, "Enables support for ExFAT");
-        add_config_option(b, &config, .filesystem_trust, "Sets which values in the FSINFO structure you trust.");
-        add_config_option(b, &config, .lock, "Enables file system locking");
-        add_config_option(b, &config, .reentrant, "Makes the library reentrant");
-        add_config_option(b, &config, .sync_type, "Defines the name of the C type which is used for sync operations");
-        add_config_option(b, &config, .timeout, "Defines the timeout period in OS ticks");
+        add_config_option(b, &config, "read_only", "If set, the library will only be able to read filesystems.");
+        add_config_option(b, &config, "minimize", "Set this to different values to reduce API surface");
+        add_config_option(b, &config, "find", "Enable find support");
+        add_config_option(b, &config, "mkfs", "Enable mkfs support");
+        add_config_option(b, &config, "fastseek", "Use fast seek option");
+        add_config_option(b, &config, "expand", "Enables expandfs support");
+        add_config_option(b, &config, "chmod", "Enables chmod support");
+        add_config_option(b, &config, "label", "Enables support for handling of filesystem labels");
+        add_config_option(b, &config, "forward", "Enables forward support");
+        add_config_option(b, &config, "strfuncs", "Enables file string functions");
+        add_config_option(b, &config, "printf_lli", "Enables printf_lli support");
+        add_config_option(b, &config, "printf_float", "Enables printf_float support");
+        add_config_option(b, &config, "strf_encoding", "Sets the format string encoding");
+        add_config_option(b, &config, "max_long_name_len", "Sets the maximum name for long file names");
+        add_config_option(b, &config, "code_page", "Defines the OEM code page");
+        add_config_option(b, &config, "long_file_name", "Enables long file name support");
+        add_config_option(b, &config, "long_file_name_encoding", "Sets the encoding for long file names");
+        add_config_option(b, &config, "long_file_name_buffer_size", "Sets the buffer size for long file names");
+        add_config_option(b, &config, "short_file_name_buffer_size", "Sets the buffer size for short file names");
+        add_config_option(b, &config, "relative_path_api", "Enables the relative path API");
+        add_config_option(b, &config, "multi_partition", "Enables support for several partitions on the same drive");
+        add_config_option(b, &config, "lba64", "Enables support for 64 bit linear block addresses.");
+        add_config_option(b, &config, "min_gpt_sectors", "Sector count threshold for switching to GPT partition tables");
+        add_config_option(b, &config, "use_trim", "Enables support for ATA TRIM command");
+        add_config_option(b, &config, "tiny", "Enable tiny buffer configuration");
+        add_config_option(b, &config, "exfat", "Enables support for ExFAT");
+        add_config_option(b, &config, "filesystem_trust", "Sets which values in the FSINFO structure you trust.");
+        add_config_option(b, &config, "lock", "Enables file system locking");
+        add_config_option(b, &config, "reentrant", "Makes the library reentrant");
+        add_config_option(b, &config, "sync_type", "Defines the name of the C type which is used for sync operations");
+        add_config_option(b, &config, "timeout", "Defines the timeout period in OS ticks");
 
         const maybe_rtc_time = b.option([]const u8, "static-rtc", "Disables runtime time API by setting the clock to a fixed value. Provide a string in the format YYYY-MM-DD");
         if (maybe_rtc_time) |rtc_time| {
@@ -273,7 +273,7 @@ fn add_config_field(config_header: *std.Build.Step.ConfigHeader, config: Config,
 }
 
 fn add_config_option(b: *std.Build, config: *Config, comptime field: []const u8, desc: []const u8) void {
-    const T = @FieldType(Config, @tagName(field));
+    const T = @FieldType(Config, field);
     if (b.option(T, field, desc)) |value|
         @field(config, field) = value;
 }
